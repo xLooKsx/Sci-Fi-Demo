dd4052ffe6edebb19677e2913ad2f3d529960b00
xLooKs_ <lu.oliveira.silva98@gmail.com>
2022-06-14 12:01:58 -0300

    small inventory added to player with an item too

--- Sci-Fi Demo/Assets/Game/Scripts/UiManager.cs
+++ Sci-Fi Demo/Assets/Game/Scripts/UiManager.cs
@@ -1,14 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UiManager : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI ammoText;
- public void updateAmmoText(int ammo)
+
+    [SerializeField]
+    private TextMeshProUGUI interactionText;
+
+    [SerializeField]
+    private Image _coinImage;
+
+    private void Start()
+    {
+        //interactionText.enabled = false;
+    }
+    public void updateAmmoText(int ammo)
     {
         ammoText.text = "Ammo: " + ammo;
+    }
+
+    public void showInteractionText()
+    {
+        interactionText.gameObject.SetActive(true);
+    }
+
+    public void hiddenInteractionText()
+    {
+        interactionText.gameObject.SetActive(false);
+    }
+
+    public void showCoinImage()
+    {
+        _coinImage.gameObject.SetActive(true);
+    }
+
+    public void hideCoinImage()
+    {
+        _coinImage.gameObject.SetActive(false);
     }
 }
